20bd8d2d4cf0ad06ef38443e2a31b210fa40c483
Hidetoshi Imai <hidepin@gmail.com>
2018-07-17 21:17:31 +0900

    Add check ext.

--- msoffice-illegal-charachecker.py
+++ msoffice-illegal-charachecker.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import re
 import docx
 
 args = sys.argv
-
-doc = docx.Document(args[1])
-
 illegal_chars = '[０-９ａ-ｚＡ-Ｚ　！”＃＄％＆｀（）＊＋：；＜＞＝？]'
 
-illegal_line = re.compile(r'{}'.format(illegal_chars))
+base, ext = os.path.splitext(args[1])
 
-for doc_paagraphs in doc.paragraphs:
-    match = illegal_line.search(doc_paagraphs.text)
-    if match != None:
-        print(args[1] + "," + re.sub(r'({})'.format(illegal_chars), r'[\1]', doc_paagraphs.text))
+if ext == '.docx':
+    doc = docx.Document(args[1])
+
+    illegal_line = re.compile(r'{}'.format(illegal_chars))
+
+    for doc_paagraphs in doc.paragraphs:
+        match = illegal_line.search(doc_paagraphs.text)
+        if match != None:
+            print(args[1] + "," + re.sub(r'({})'.format(illegal_chars), r'[\1]', doc_paagraphs.text))
