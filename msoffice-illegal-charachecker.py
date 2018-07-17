78f54f3352e60d1f3907c0fa4ed5f558dc3a515c
Hidetoshi Imai <hidepin@gmail.com>
2018-07-17 21:31:38 +0900

    Fix docx variable.

--- msoffice-illegal-charachecker.py
+++ msoffice-illegal-charachecker.py
@@ -8,9 +8,9 @@ import docx
 illegal_chars = '[０-９ａ-ｚＡ-Ｚ　！”＃＄％＆｀（）＊＋：；＜＞＝？]'
 
 def check_docx(filename):
-    docx = docx.Document(filename)
+    doc = docx.Document(filename)
     illegal_line = re.compile(r'{}'.format(illegal_chars))
-    for doc_paagraphs in docx.paragraphs:
+    for doc_paagraphs in doc.paragraphs:
         match = illegal_line.search(doc_paagraphs.text)
         if match != None:
             print(args[1] + "," + re.sub(r'({})'.format(illegal_chars), r'[\1]', doc_paagraphs.text))
