4fa65ce03969a7d1b5b8f919e8ac88923bead38f
Hidetoshi Imai <hidepin@gmail.com>
2018-07-17 20:22:00 +0900

    Add sub setting.

--- msoffice-illegal-charachecker.py
+++ msoffice-illegal-charachecker.py
@@ -8,9 +8,9 @@ args = sys.argv
 
 doc = docx.Document(args[1])
 
-illegal_line = re.compile(r'[０-９ａ-ｚＡ-Ｚ　！”＃＄％＆｀（）＊＋ー：；＜＞＝？]')
+illegal_line = re.compile(r'[０-９ａ-ｚＡ-Ｚ　！”＃＄％＆｀（）＊＋：；＜＞＝？]')
 
 for doc_paagraphs in doc.paragraphs:
     match = illegal_line.search(doc_paagraphs.text)
     if match != None:
-        print(args[1] + "," + doc_paagraphs.text)
+        print(args[1] + "," + re.sub(r'([０-９ａ-ｚＡ-Ｚ　！”＃＄％＆｀（）＊＋：；＜＞＝？])', r'[\1]', doc_paagraphs.text))
